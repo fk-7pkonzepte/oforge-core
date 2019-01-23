8849fb466b2b19eeffddea031a7c21bb9d4caa18
FK <fk@7pkonzepte.de>
2019-01-23 23:04:04 +0100

    phpstormmeta for core services

--- Engine/Modules/Core/.phpstorm/.phpstorm.meta.php
+++ Engine/Modules/Core/.phpstorm/.phpstorm.meta.php
@@ -4,7 +4,13 @@ namespace PHPSTORM_META {
 
     if (function_exists('override')) {
         override(\Oforge\Engine\Modules\Core\Manager\Services\ServiceManager::get(0), map([
-            'ping' => \Oforge\Engine\Modules\Core\Services\PingService::class,
+            'config'         => \Oforge\Engine\Modules\Core\Services\ConfigService::class,
+            'endpoint'       => \Oforge\Engine\Modules\Core\Services\EndpointService::class,
+            'middleware'     => \Oforge\Engine\Modules\Core\Services\MiddlewareService::class,
+            'ping'           => \Oforge\Engine\Modules\Core\Services\PingService::class,
+            'plugin.access'  => \Oforge\Engine\Modules\Core\Services\PluginAccessService::class,
+            'plugin.state'   => \Oforge\Engine\Modules\Core\Services\PluginStateService::class,
+            'store.keyvalue' => \Oforge\Engine\Modules\Core\Services\KeyValueStoreService::class,
         ]));
     }
 
