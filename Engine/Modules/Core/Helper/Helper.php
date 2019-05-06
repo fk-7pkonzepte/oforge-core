631002c6ae9412ef33fe9904ce37f61a7206a193
FK <fk@7pkonzepte.de>
2019-05-06 15:28:09 +0200

    Refactoring Helper

--- Engine/Modules/Core/Helper/Helper.php
+++ Engine/Modules/Core/Helper/Helper.php
@@ -4,102 +4,15 @@ namespace Oforge\Engine\Modules\Core\Helper;
 
 use Oforge\Engine\Modules\Core\Abstracts\AbstractBootstrap;
 use Oforge\Engine\Modules\Core\Exceptions\InvalidClassException;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
 
+/**
+ * Class Helper
+ *
+ * @package Oforge\Engine\Modules\Core\Helper
+ * @deprecated
+ */
 class Helper {
     protected static $omitFolders = [".", "..", "vendor", "var"];
-
-    /**
-     * Get all directories recursive based on the defined path, except the folders to omit
-     *
-     * @param string $path
-     *
-     * @return array
-     */
-    public static function getAllDirs(string $path) {
-        $result = [];
-        $tmp    = scandir($path);
-
-        foreach ($tmp as $dir) {
-            $v = $path . DIRECTORY_SEPARATOR . $dir;
-
-            if (is_dir($v) && !in_array($dir, self::$omitFolders)) {
-                array_push($result, $v);
-                $result = array_merge($result, Helper::getAllDirs($v));
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * get all Bootstrap.php files inside a defined path
-     *
-     * @param string $path
-     *
-     * @return array
-     */
-    public static function getBootstrapFiles(string $path) {
-        $result = [];
-
-        $cacheFile = ROOT_PATH . Statics::CACHE_DIR . DIRECTORY_SEPARATOR . basename($path) . ".cache";
-
-        if (file_exists($cacheFile) && Oforge()->Settings()->get("mode") != "development") {
-            $result = unserialize(file_get_contents($cacheFile));
-        } else {
-            $result = self::findFilesInPath($path, "bootstrap.php");
-            file_put_contents($cacheFile, serialize($result));
-        }
-
-        return $result;
-    }
-
-    /**
-     * get all template files inside a defined path
-     *
-     * @param string $path
-     *
-     * @return array
-     */
-    public static function getTemplateFiles(string $path) {
-
-        $cacheFile = ROOT_PATH . Statics::CACHE_DIR . DIRECTORY_SEPARATOR . basename($path) . ".cache";
-
-        if (file_exists($cacheFile) && Oforge()->Settings()->get("mode") != "development") {
-            $result = unserialize(file_get_contents($cacheFile));
-        } else {
-            $result = self::findFilesInPath($path, "template.php");
-            file_put_contents($cacheFile, serialize($result));
-        }
-
-        return $result;
-    }
-
-    /**
-     * search for filename inside a path
-     *
-     * @param $path string
-     * @param $fileName string
-     *
-     * @return array
-     */
-    private static function findFilesInPath($path, $fileName) {
-        $result = [];
-
-        $path = realpath($path);
-        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $f) {
-            if (strtolower($f->getFileName()) == $fileName) {
-
-                $classpath = str_replace($path . DIRECTORY_SEPARATOR, "", $f->getPath());
-                $classpath = str_replace(".php", "", $classpath);
-
-                $result[$classpath] = $f->getPath() . DIRECTORY_SEPARATOR . $f->getFileName();
-            }
-        }
-
-        return $result;
-    }
 
     /**
      * get the instance of a module/plugin based on the bootstrap file
@@ -119,21 +32,6 @@ class Helper {
     }
 
     /**
-     * @return string
-     */
-    public static function generateGuid() {
-        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
-            mt_rand(0, 65535),
-            mt_rand(0, 65535),
-            mt_rand(0, 65535),
-            mt_rand(16384, 20479),
-            mt_rand(32768, 49151),
-            mt_rand(0, 65535),
-            mt_rand(0, 65535),
-            mt_rand(0, 65535));
-    }
-
-    /**
      * Get some metadata from a file.
      * If the file is a psr4 conform class, the function finds the namespace and classname and returns them as an array.
      * Otherwise null.
@@ -143,37 +41,40 @@ class Helper {
      * @return array|null
      */
     public static function getFileMeta(string $fileName) {
-
-        $file = fopen($fileName, 'r');
-        $class = '';
+        $file      = fopen($fileName, 'r');
+        $class     = '';
         $namespace = '';
-        $buffer = '';
-        $i = 0;
-        $fileMeta = [];
+        $buffer    = '';
+        $i         = 0;
+        $fileMeta  = [];
 
         while (!$class) {
-            if (feof($file)) break;
+            if (feof($file)) {
+                break;
+            }
 
             $buffer .= fread($file, 512);
             $tokens = token_get_all($buffer);
 
-            if (strpos($buffer, '{') === false) continue;
+            if (strpos($buffer, '{') === false) {
+                continue;
+            }
 
-            for (;$i < count($tokens); $i++) {
+            for (; $i < count($tokens); $i++) {
                 if ($tokens[$i][0] === T_NAMESPACE) {
-                    for ($j=$i+1;$j<count($tokens); $j++) {
+                    for ($j = $i + 1; $j < count($tokens); $j++) {
                         if ($tokens[$j][0] === T_STRING) {
-                            $namespace .= '\\'.$tokens[$j][1];
-                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                            $namespace .= '\\' . $tokens[$j][1];
+                        } elseif ($tokens[$j] === '{' || $tokens[$j] === ';') {
                             break;
                         }
                     }
                 }
 
                 if ($tokens[$i][0] === T_CLASS) {
-                    for ($j=$i+1;$j<count($tokens);$j++) {
+                    for ($j = $i + 1; $j < count($tokens); $j++) {
                         if ($tokens[$j] === '{') {
-                            $class = $tokens[$i+2][1];
+                            $class = $tokens[$i + 2][1];
                         }
                     }
                 }
@@ -198,4 +99,5 @@ class Helper {
 
         return $fileMeta;
     }
+
 }
--- Engine/Modules/Core/Helper/StringHelper.php
+++ Engine/Modules/Core/Helper/StringHelper.php
@@ -2,23 +2,21 @@
 
 namespace Oforge\Engine\Modules\Core\Helper;
 
+/**
+ * StringHelper
+ *
+ * @package Oforge\Engine\Modules\Core\Helper
+ */
 class StringHelper {
-    /**
-     * Check if a string starts with a given value
-     *
-     * @param string $haystack
-     * @param string $needle
-     *
-     * @return bool
-     */
-    public static function startsWith(string $haystack, string $needle) {
-        $length = strlen($needle);
 
-        return (substr($haystack, 0, $length) === $needle);
+    /**
+     * Prevent instance.
+     */
+    private function __construct() {
     }
 
     /**
-     * Check if a string ends with a given value
+     * Check if a string ends with a given value. If needle empty then true.
      *
      * @param string $haystack
      * @param string $needle
@@ -27,7 +25,7 @@ class StringHelper {
      */
     public static function endsWith(string $haystack, string $needle) {
         $length = strlen($needle);
-        if ($length == 0) {
+        if ($length === 0) {
             return true;
         }
 
@@ -35,28 +33,44 @@ class StringHelper {
     }
 
     /**
-     * Check if a given value is inside a String
+     * Check if a string starts with a given value
      *
      * @param string $haystack
      * @param string $needle
      *
      * @return bool
      */
-    public static function contains(string $haystack, string $needle) {
+    public static function startsWith(string $haystack, string $needle) : bool {
+        $length = strlen($needle);
+        if ($length === 0) {
+            return true;
+        }
+
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    /**
+     * Check if a given value is inside a string.
+     *
+     * @param string $haystack
+     * @param string $needle
+     *
+     * @return bool
+     */
+    public static function contains(string $haystack, string $needle) : bool {
         return (strpos($haystack, $needle) !== false);
     }
 
     /**
      * Check if a value is found before a given string / character.
-     * If found, return that value.
-     * Otherwise return the haystack
+     * If found return that value, otherwise return the haystack
      *
      * @param string $haystack The part where you search inside
      * @param string $needle The separator
      *
-     * @return mixed
+     * @return string
      */
-    public static function substringBefore(string $haystack, string $needle) {
+    public static function substringBefore(string $haystack, string $needle) : string {
         if (StringHelper::contains($haystack, $needle)) {
             return explode($needle, $haystack)[0];
         }
